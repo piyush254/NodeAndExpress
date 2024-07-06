b4b92412f4d62fed14fc8b42058058e338befc5b
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-07-06 09:29:15 +0530

    Node and express

--- update.js
+++
@@ -1,8 +0,0 @@
-const dbConnect = require('./mongoDB')
-
-const update =  async ()=> {
- let data = await dbConnect();
- let result = await data.updateOne({  name: 'Realme 8' },{$set : {price:20000}});
- console.warn(result.acknowledged);
-}
-update();
